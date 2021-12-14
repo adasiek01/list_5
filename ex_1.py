e2e6aa4a2aaaef40151f448463804afac36d283d
Adam Kawałko <262329@student.pwr.edu.pl>
2021-12-15 00:07:23 +0100

    coś tam

--- ex_1.py
+++ ex_1.py
@@ -1,8 +1,15 @@
 import numpy as np
-a = np.array([[3, 2, 0], [1, -1, 0], [0, 5, 1]])
-b = np.array([2, 4, -1])
-from scipy import linalg
-x = linalg.solve(a, b)
-array([ 2., -2., 9.])
-np.dot(a, x) == b #sprawdzenie!!!
-array([True, True, True], dtype=bool)
+
+
+def sol():
+    counter = 0
+    a = np.array([[3, 2, 0], [1, -1, 0], [0, 5, 1]])
+    b = np.array([2, 4, -1])
+    from scipy import linalg
+    x = linalg.solve(a, b)
+    counter = counter + len(a)*3
+    return x, np.dot(a, x) == b, counter
+
+
+if __name__ == "__main__":
+    print(sol())
